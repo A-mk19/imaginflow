24365a558a2896909be326cf93c8291ef7df7f4c
A-mk19 <amindhyala.mk@gmail.com>
2026-06-10 01:38:32 +0530

    Created editor UI and canvas controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,165 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
+import { PanelLeftClose, PanelRightClose, PanelBottomClose, Undo2, Redo2, Check, Eye, Download } from "lucide-react"
 
+
+// {brand and header options section}
 function Header() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1 style={{ color: "#a78bfa" }}>Imaginflow.io</h1>
-      <p style={{ color: "#888", fontSize: "14px" }}>
-        Visual app builder
-      </p>
+    <div className="flex px-4 h-16 bg-slate-950 items-center">
+
+      {/* Left - Brand block */}
+      <div className="flex">
+        <img className="w-15 h-15" src="image copy.png" alt="" />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-xl font-semibold text-violet-500">Imaginflow.io</h1>
+          <p className="text-gray-400 text-xs text-right font-semibold">imagin to build</p>
+        </div>
+      </div>
+
+      {/* center-project-title */}
+      <div className="flex-1 text-center">
+        <h2 className="text-gray-50">Project Name</h2>
+      </div>
+
+      {/* right buttons */}
+      <div className="flex items-center gap-4 text-center ">
+        <Undo2 className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+        <Redo2 className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+        <PanelLeftClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+        <PanelRightClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+        <PanelBottomClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+        <button className="bg-green-500  hover:bg-gray-50 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Check size={14} /></button>
+        <button className="bg-yellow-500 hover:bg-gray-50 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Eye size={14} /></button>
+        <button className="bg-blue-500 hover:bg-gray-800 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Download size={14} /></button>
+      </div>
+
+    </div >
+  )
+}
+
+// tool-bar props
+function Tools(props) {
+  return (
+    <div>
+      <h1 className={`font-semibold cursor-pointer px-3 py-1 text-xs  rounded ${props.isSelected ? "bg-white text-black" : "text-white"}`} onClick={() => props.onSelect(props.name)}>{props.name}</h1>
+    </div >
+  )
+}
+
+
+//Toolbar tab-options
+
+function ToolBar(props) {
+  return (
+    <div className="flex bg-slate-950 h-8 gap-5 items-center p-2 border-y-1 border-gray-50">
+      <Tools name="Components" isSelected={props.selectedTools === "Components"} onSelect={props.onSelect} />
+      <Tools name="Design" isSelected={props.selectedTools === "Design"} onSelect={props.onSelect} />
+      <Tools name="Functions" isSelected={props.selectedTools === "Functions"} onSelect={props.onSelect} />
+      <Tools name="Connections" isSelected={props.selectedTools === "Connections"} onSelect={props.onSelect} />
+      <Tools name="Execution" isSelected={props.selectedTools === "Execution"} onSelect={props.onSelect} />
+      <Tools name="Backend" isSelected={props.selectedTools === "Backend"} onSelect={props.onSelect} />
+      <Tools name="Database" isSelected={props.selectedTools === "Database"} onSelect={props.onSelect} />
     </div>
   )
 }
 
-function ToolItems(props) {
+// {SideBar component}
+function SideBar(props) {
   return (
-    <p onClick={() => props.onSelect(props.name)}
-      style={{
-        color: props.isSelected ? "#fff" : "#888", fontSize: "14px"
-      }}> {props.name}</p >
-  )
-}
-
-function Sidebar(props) {
-
-  return (
-    <div style={{ padding: "20px" }}>
-      <h3 style={{ color: "#a78bfa" }}>Tools</h3>
-      <p>{props.selectedTool}</p>
-      <ToolItems name="Select" isSelected={props.selectedTool === "Select"} onSelect={props.onSelect} />
-      <ToolItems name="Rectangle" isSelected={props.selectedTool === "Rectangle"} onSelect={props.onSelect} />
-      <ToolItems name="Text" isSelected={props.selectedTool === "Text"} onSelect={props.onSelect} />
-      <ToolItems name="Circle" isSelected={props.selectedTool === "Circle"} onSelect={props.onSelect} />
+    // selected tool
+    <div className="flex h-full w-50 bg-slate-950 flex-col p-2">
+      <div className="p-1">
+        <h3 className="text-gray-50">{props.selectedTools}</h3>
+      </div>
+      {/* drop downfor tools catogory */}
+      <div className="w-full h-full border-2 border-gray-50 bg-slate-900 align-middle ">
+        <div className="w-full h-5 border-2 border-gray-50 bg-gray-50 ">
+          <p className="text-xs">Drop down for catogory tools</p>
+        </div>
+        {/* tools and components will appear here */}
+        <div className="w-full h-full flex justify-center items-center">
+          <p className="text-gray-50 text-center ">all the respected Drag drop UI componets and tools will appers here as per tools and catogory</p>
+        </div>
+      </div>
     </div>
   )
 }
+
+
+//canvas function
+function Canvas() {
+  //useState for zoom with scroll wheel ans shift<-->
+  const [zoom, setZoom] = useState(1)
+  const canvasRef = useRef(null)
+  //zoom function
+  function handleZoom(e) {
+    e.preventDefault();
+
+    // SHIFT + SCROLL = horizontal scroll
+    if (e.shiftKey) {
+      canvasRef.current.scrollLeft += e.deltaY
+      return
+    }
+    // zoomin-zoomout scroll wheel condition
+    if (e.deltaY < 0) {
+      setZoom(prev => Math.min(prev + 0.1, 3))    // scroll up = zoom IN, max 3x
+    } else {
+      setZoom(prev => Math.max(prev - 0.1, 0.2))  // scroll down = zoom OUT, min 0.2x
+    }
+  }
+  return (
+    // canavs parent-tag
+    <div className="flex flex-1 h-full w-full bg-gray-50 ">
+      {/* white board */}
+      <div ref={canvasRef} className="flex flex-1 bg-white justify-center items-center overflow-auto cursor-crosshair p-2 relative " onWheel={handleZoom} >
+        {/* <div  onWheel={handelZoom}> */}
+        <div style={{ transform: `scale(${zoom})`, transformOrigin: "top left", width: "100%", height: "100%" }}>
+          <p className="text-black">test text</p>
+        </div>
+        <div className="left-1/2 -translate-x-1/2 flex w-[60%] bg-slate-950 h-10 rounded-lg shadow-lg shadow-gray-700  fixed bottom-4 opacity-20 hover:opacity-100">
+
+        </div>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+function RightSidebar(props) {
+  return (
+    // selected tool
+    <div className="flex h-full w-50 bg-slate-950 flex-col p-2">
+      <p>properties</p>
+    </div>
+  )
+}
+// sidebar + canvas + propertis bar
+function Middle(props) {
+  return (
+    <div className="flex flex-row flex-1 overflow-hidden">
+      <SideBar selectedTools={props.selectedTools} />
+      <Canvas />
+      <RightSidebar />
+    </div>
+  )
+}
+
+
+
+
+
+
 
 function App() {
-  const [selectedTool, setSelectedTool] = useState("Select")
-
+  // usestate for Toolbar-props
+  const [selectedTools, setSelectedTools] = useState("Components")
   return (
-    <div >
+    <div className="flex flex-col h-screen ">
       <Header />
-      <p>{selectedTool}</p>
-      <Sidebar selectedTool={selectedTool} onSelect={setSelectedTool} />
+      <ToolBar selectedTools={selectedTools} onSelect={setSelectedTools} />
+      <Middle selectedTools={selectedTools} setSelectedTools={setSelectedTools} />
     </div>
   )
 }
---
+++ src/test.jsx
@@ -0,0 +1,163 @@
+import { useState, useRef } from "react"
+import { PanelLeftClose, PanelRightClose, PanelBottomClose, Undo2, Redo2, Check, Eye, Download } from "lucide-react"
+function Header() {
+    return (
+        <div className="flex px-4 h-16 bg-slate-950 items-center">
+
+            {/* Left - Brand block */}
+            <div className="flex">
+                <img className="w-15 h-15" src="image copy.png" alt="" />
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-xl font-semibold text-violet-500">Imaginflow.io</h1>
+                    <p className="text-gray-400 text-xs text-right font-semibold">imagin to build</p>
+                </div>
+            </div>
+
+            {/* center-project-title */}
+            <div className="flex-1 text-center">
+                <h2 className="text-gray-50">Project Name</h2>
+            </div>
+
+            {/* right buttons */}
+            <div className="flex items-center gap-4 text-center ">
+                <Undo2 className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+                <Redo2 className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+                <PanelLeftClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+                <PanelRightClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+                <PanelBottomClose className="text-gray-400 cursor-pointer hover:text-white" size={18} />
+                <button className="bg-green-500  hover:bg-gray-50 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Check size={14} /></button>
+                <button className="bg-yellow-500 hover:bg-gray-50 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Eye size={14} /></button>
+                <button className="bg-blue-500 hover:bg-gray-800 px-4 py-1 rounded-lg flex items-center justify-center gap-1"><Download size={14} /></button>
+            </div>
+
+        </div >
+    )
+}
+
+// tool-bar props
+function Tools(props) {
+    return (
+        <div>
+            <h1 className={`font-semibold cursor-pointer px-3 py-1 text-xs  rounded ${props.isSelected ? "bg-white text-black" : "text-white"}`} onClick={() => props.onSelect(props.name)}>{props.name}</h1>
+        </div >
+    )
+}
+
+
+//Toolbar tab-options
+
+function ToolBar(props) {
+    return (
+        <div className="flex bg-slate-950 h-8 gap-5 items-center p-2 border-y-1 border-gray-50">
+            <Tools name="Components" isSelected={props.selectedTools === "Components"} onSelect={props.onSelect} />
+            <Tools name="Design" isSelected={props.selectedTools === "Design"} onSelect={props.onSelect} />
+            <Tools name="Functions" isSelected={props.selectedTools === "Functions"} onSelect={props.onSelect} />
+            <Tools name="Connections" isSelected={props.selectedTools === "Connections"} onSelect={props.onSelect} />
+            <Tools name="Execution" isSelected={props.selectedTools === "Execution"} onSelect={props.onSelect} />
+            <Tools name="Backend" isSelected={props.selectedTools === "Backend"} onSelect={props.onSelect} />
+            <Tools name="Database" isSelected={props.selectedTools === "Database"} onSelect={props.onSelect} />
+        </div>
+    )
+}
+
+// {SideBar component}
+function SideBar(props) {
+    return (
+        // selected tool
+        <div className="flex h-full w-50 bg-slate-950 flex-col p-2">
+            <div className="p-1">
+                <h3 className="text-gray-50">{props.selectedTools}</h3>
+            </div>
+            {/* drop downfor tools catogory */}
+            <div className="w-full h-full border-2 border-gray-50 bg-slate-900 align-middle ">
+                <div className="w-full h-5 border-2 border-gray-50 bg-gray-50 ">
+                    <p className="text-xs">Drop down for catogory tools</p>
+                </div>
+                {/* tools and components will appear here */}
+                <div className="w-full h-full flex justify-center items-center">
+                    <p className="text-gray-50 text-center ">all the respected Drag drop UI componets and tools will appers here as per tools and catogory</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Canvas() {
+    const [zoom, setZoom] = useState(1)
+    const canvasRef = useRef(null)
+
+    function handleZoom(e) {
+        e.preventDefault();
+
+        // SHIFT + SCROLL = horizontal scroll
+        if (e.shiftKey) {
+            canvasRef.current.scrollLeft += e.deltaY
+            return
+        }
+
+        if (e.deltaY < 0) {
+            setZoom(prev => Math.min(prev + 0.1, 3))    // scroll up = zoom IN, max 3x
+        } else {
+            setZoom(prev => Math.max(prev - 0.1, 0.2))  // scroll down = zoom OUT, min 0.2x
+        }
+    }
+    return (
+        <div className="flex flex-1 h-full w-full bg-gray-50 ">
+            {/* <p>canvas</p> */}
+            {/* white board */}
+            <div ref={canvasRef} className="flex flex-1 bg-white justify-center items-center overflow-auto cursor-crosshair p-2 relative " onWheel={handleZoom} >
+                {/* <div  onWheel={handelZoom}> */}
+                <div style={{ transform: `scale(${zoom})`, transformOrigin: "top left", width: "100%", height: "100%" }}>
+                    <p className="text-black">test text</p>
+                </div>
+                <div className="left-1/2 -translate-x-1/2 flex w-[60%] bg-slate-950 h-10 rounded-lg shadow-lg shadow-gray-700  fixed bottom-4 opacity-20 hover:opacity-100">
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+function RightSidebar(props) {
+    return (
+        // selected tool
+        <div className="flex h-full w-50 bg-slate-950 flex-col p-2">
+            <p>properties</p>
+        </div>
+    )
+}
+
+function Middle(props) {
+    return (
+        <div className="flex flex-row flex-1 overflow-hidden">
+            <SideBar selectedTools={props.selectedTools} />
+            <Canvas />
+            <RightSidebar />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+function App() {
+    const [selectedTools, setSelectedTools] = useState("Components")
+    const [zoom, setZoom] = useState(1)
+    return (
+        <div className="flex flex-col h-screen ">
+            <Header />
+            <ToolBar selectedTools={selectedTools} onSelect={setSelectedTools} />
+            {/* <SideBar selectedTools={selectedTools} setSelectedTools={setSelectedTools} />
+      <Canvas /> */}
+            <Middle selectedTools={selectedTools} setSelectedTools={setSelectedTools} />
+        </div>
+    )
+}
+
+export default App
